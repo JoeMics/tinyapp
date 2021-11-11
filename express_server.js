43a2cca4ea156b9869dac25a622b42e3c62d6fbc
Joseph Micla <josephmicla@gmail.com>
2021-11-11 22:51:12 +0000

    moved helper functions to helpers.js and required them into express_server.js

--- express_server.js
+++ express_server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
+const {
+  generateRandomString,
+  findUserByCookie,
+  findUserByEmail,
+  urlsForUser,
+} = require('./helpers');
 
 const PORT = 8080; // default port 8080
 
@@ -36,34 +42,6 @@ const users = {
     email: 'a@a',
     password: '$2b$10$n.xHo8DCl9v/Pnd9.pqrHOMz4bCTeu49EeyOeRvM.rv46YqZEDXDy'
   }
-};
-
-const generateRandomString = () => {
-  return Math.random().toString(16).substr(2, 6);
-};
-
-const findUserByCookie = (userDB, userIdCookie) => {
-  return userDB[userIdCookie];
-};
-
-const findUserByEmail = (userDB, email) => {
-  for (const user in userDB) {
-    if (email === userDB[user].email) {
-      return userDB[user];
-    }
-  }
-};
-
-const urlsForUser = (urlDB, id) => {
-  const userURLS = {};
-  for (const url in urlDB) {
-    // check to see if the url's userID === id
-    if (urlDB[url].userID === id) {
-      //  if it is, add it to the new object
-      userURLS[url] = urlDB[url];
-    }
-  }
-  return userURLS;
 };
 
 app.get("/", (req, res) => {
