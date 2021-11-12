878eee84d7669a160e6fa1ad45f431b610406e08
Joseph Micla <josephmicla@gmail.com>
2021-11-12 00:37:09 +0000

    added assertions to test findUserByEmail to make sure it returns the correct email object

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -30,3 +30,21 @@ const users = {
     password: '$2b$10$n.xHo8DCl9v/Pnd9.pqrHOMz4bCTeu49EeyOeRvM.rv46YqZEDXDy'
   }
 };
+
+describe('#findUserByEmail', () => {
+  it("should return the user object by it's email", () => {
+    const userObj = findUserByEmail(users, 'b@b');
+    const expectedUser = {
+      id: '5a0fae',
+      email: 'b@b',
+      password: '$2b$10$BiMgdxbzvtfdO4wIKEP5deqN8Vk0HF0m4FhUnzYK7Z2tJnCVROcJG'
+    };
+
+    assert.deepEqual(userObj, expectedUser);
+  });
+
+  it('should return undefined if no match', () => {
+    const nonExistantUser = findUserByEmail(users, 'not@real.email');
+    assert.isUndefined(nonExistantUser);
+  });
+});
