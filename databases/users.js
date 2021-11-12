8a7f1508dd90a5e91147e694036f99daaec29a88
Joseph Micla <josephmicla@gmail.com>
2021-11-12 03:42:01 +0000

    moved users to /databases/users.js, required into express_server

---
+++ databases/users.js
@@ -0,0 +1,12 @@
+module.exports = {
+  '5a0fae': {
+    id: '5a0fae',
+    email: 'b@b',
+    password: '$2b$10$BiMgdxbzvtfdO4wIKEP5deqN8Vk0HF0m4FhUnzYK7Z2tJnCVROcJG'
+  },
+  '049b06': {
+    id: '049b06',
+    email: 'a@a',
+    password: '$2b$10$n.xHo8DCl9v/Pnd9.pqrHOMz4bCTeu49EeyOeRvM.rv46YqZEDXDy'
+  }
+};
