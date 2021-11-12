2ad43d2235ce5fc7288e63cb5397ad03c15f9187
Joseph Micla <josephmicla@gmail.com>
2021-11-12 03:39:02 +0000

    moved urlDatabase to /databases/urlDatabase.js, required into express_server

---
+++ databases/urlDatabase.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "049b06"
+  },
+  "Ksm5xK": {
+    longURL: "http://www.google.com",
+    userID: "5a0fae"
+  }
+};
