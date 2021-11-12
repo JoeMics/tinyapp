938156796ed9be9f609518432364816c3a6f21ef
Joseph Micla <josephmicla@gmail.com>
2021-11-12 05:22:32 +0000

    removed 'databases' from express_server and required into it instead

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const {
   findUserByEmail,
   urlsForUser,
 } = require('./helpers');
+const urlDatabase = require('./databases/urlDatabase');
+const users = require('./databases/users');
 
 const PORT = 8080; // default port 8080
 
@@ -20,29 +22,7 @@ app.use(cookieSession({
 }));
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "049b06"
-  },
-  "Ksm5xK": {
-    longURL: "http://www.google.com",
-    userID: "5a0fae"
-  }
-};
 
-const users = {
-  '5a0fae': {
-    id: '5a0fae',
-    email: 'b@b',
-    password: '$2b$10$BiMgdxbzvtfdO4wIKEP5deqN8Vk0HF0m4FhUnzYK7Z2tJnCVROcJG'
-  },
-  '049b06': {
-    id: '049b06',
-    email: 'a@a',
-    password: '$2b$10$n.xHo8DCl9v/Pnd9.pqrHOMz4bCTeu49EeyOeRvM.rv46YqZEDXDy'
-  }
-};
 
 app.get("/", (req, res) => {
   const user = findUserByCookie(users, req.session.userID);
